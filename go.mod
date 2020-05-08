dfcdf0cf0094f0931a933db3d73e8caec9b19250
Dewmal <dewmalnilanka@gmail.com>
2020-05-08 21:32:39 +0530

    Add config details

--- go.mod
+++ go.mod
@@ -13,4 +13,5 @@ require (
 	github.com/lib/pq v1.5.1
 	golang.org/x/net v0.0.0-20200505041828-1ed23360d12c // indirect
 	google.golang.org/appengine v1.6.6 // indirect
+	gopkg.in/yaml.v2 v2.2.8
 )
--- main.go
+++ main.go
@@ -1,25 +1,106 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/gocolly/colly/v2"
 	"github.com/google/uuid"
 	"github.com/jinzhu/gorm"
+	"gopkg.in/yaml.v2"
+	"log"
 	"net/url"
+	"os"
 	"time"
 	"webcrawler/cmd/data"
 	"webcrawler/cmd/extractor"
 )
 
+type Config struct {
+	Crawler struct {
+		DB struct {
+			Host     string `yaml:"host"`
+			Port     string `yaml:"port"`
+			UserName string `yaml:"username"`
+			Password string `yaml:"password"`
+			DataBase string `yaml:"dataBase"`
+		}
+	}
+}
+
+// NewConfig returns a new decoded Config struct
+func NewConfig(configPath string) (*Config, error) {
+	// Create config structure
+	config := &Config{}
+
+	// Open config file
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Init new YAML decode
+	d := yaml.NewDecoder(file)
+
+	// Start YAML decoding from file
+	if err := d.Decode(&config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+func ValidateConfigPath(path string) error {
+	s, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	if s.IsDir() {
+		return fmt.Errorf("'%s' is a directory, not a normal file", path)
+	}
+	return nil
+}
+
+// ParseFlags will create and parse the CLI flags
+// and return the path to be used elsewhere
+func ParseFlags() (string, error) {
+	// String that contains the configured configuration path
+	var configPath string
+
+	// Set up a CLI flag called "-config" to allow users
+	// to supply the configuration file
+	flag.StringVar(&configPath, "config", os.Getenv("CRAWLER_CONFIG_PATH"), "path to config file")
+
+	// Actually parse the flags
+	flag.Parse()
+
+	// Validate the path first
+	if err := ValidateConfigPath(configPath); err != nil {
+		return "", err
+	}
+
+	// Return the configuration path
+	return configPath, nil
+}
+
 func main() {
+	// by the user in the flags
+	cfgPath, err := ParseFlags()
+	if err != nil {
+		log.Fatal(err)
+	}
+	cfg, err := NewConfig(cfgPath)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	dsn := url.URL{
-		User:     url.UserPassword("postgres", "dewmal91"),
+		User:     url.UserPassword(cfg.Crawler.DB.UserName, cfg.Crawler.DB.Password),
 		Scheme:   "postgres",
-		Host:     fmt.Sprintf("%s:%d", "localhost", 5432),
-		Path:     "ek_crawler_db",
+		Host:     fmt.Sprintf("%s:%s", cfg.Crawler.DB.Host, cfg.Crawler.DB.Port),
+		Path:     cfg.Crawler.DB.DataBase,
 		RawQuery: (&url.Values{"sslmode": []string{"disable"}}).Encode(),
 	}
+	log.Println(dsn.String())
 	db, err := gorm.Open("postgres", dsn.String())
 	db.LogMode(true)
 	if err != nil {
